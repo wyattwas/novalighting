79e7f835fcc2361cd74961f50e388707fc5401ae
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-21 15:23:59 +0100

    delete image from files and also remove link to fixtures

--- sql-php/delete-image.php
+++ sql-php/delete-image.php
@@ -2,5 +2,18 @@
 require('../database.php');
 $image_id = $_GET['id'];
 
-$query_images = "DELETE FROM images WHERE idimage = '".$image_id."';";
+$query_images = "SELECT url FROM images WHERE idimage = '$image_id'";
+$stmt = PDO->query($query_images);
+$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($images as $image) {
+    echo '../'.$image['url'];
+
+    unlink('../'.$image['url']);
+}
+
+$query_fixture_images = "DELETE FROM fixture_images WHERE idimage = '$image_id'";
+$stmt = PDO->query($query_fixture_images);
+
+$query_images = "DELETE FROM images WHERE idimage = '$image_id'";
 $stmt = PDO->query($query_images);
