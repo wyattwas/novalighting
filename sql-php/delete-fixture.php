6272bc2aa5f4b7d874c2154fccb0c19e31961bac
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-21 15:27:15 +0100

    remove link to images

--- sql-php/delete-fixture.php
+++ sql-php/delete-fixture.php
@@ -2,5 +2,8 @@
 require('../database.php');
 $fixture_id = $_GET['id'];
 
+$query_fixture_images = "DELETE FROM fixture_images WHERE idfixture = '$fixture_id'";
+$stmt = PDO->query($query_fixture_images);
+
 $query_images = "DELETE FROM fixtures WHERE idfixture = '".$fixture_id."';";
 $stmt = PDO->query($query_images);
