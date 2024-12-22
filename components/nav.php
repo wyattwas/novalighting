30980a6f0286bc94c88bdca07255b4a9be05d8fc
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-22 10:41:19 +0100

    adding posts

--- components/nav.php
+++ components/nav.php
@@ -1,7 +1,7 @@
 <nav class="nav" id="nav">
     <a href="../index.php">Logo</a>
     <a href="../list-fixture.php">Fixture List</a>
-    <a href="../past-projects.html">Vergangene Projekte</a>
+    <a href="../list-posts.php">Vergangene Projekte</a>
     <a href="https://www.youtube.com/watch?v=5P1qRpmoFp8&list=PL4nNPjb8kva9e8-wQPH9VLNp214bt5O-_" target="_blank">YouTube</a>
     <a href="javascript:void(0);" id="menu-toggle" onclick="togglemenu()">
         <div class="menu-toggle__bar1"></div>
