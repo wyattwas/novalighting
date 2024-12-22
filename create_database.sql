7e2a8e9ce51641f588d3ab33c396232292b2fc0a
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-22 10:15:09 +0100

    adding posts table

--- create_database.sql
+++ create_database.sql
@@ -18,23 +18,10 @@ create table fixtures
     info      varchar(500),
     primary key (idfixture)
 );
-insert into fixtures
-values ('1549sd64w846', 'Fixture 1', 'something something something'),
-       ('df4d54g56451', 'Fixture 2', 'something something something'),
-       ('184fg155485r', 'Fixture 3', 'something something something'),
-       ('fg457r8478r8', 'Fixture 4', 'something something something');
-insert into images
-values ('s2d545ef1121', 'https://c8.alamy.com/comp/2T8D0Y1/led-stage-lighting-fixtures-at-the-show-2T8D0Y1.jpg'),
-       ('21554e8f88e4', 'https://i.pinimg.com/originals/b3/b7/19/b3b71915972711fb329e07672e2095c5.jpg'),
-       ('s4d48e4fe5e4',
-        'https://thumbs.dreamstime.com/z/professional-stage-light-fixtures-hanged-truss-party-show-set-up-rehearsal-sound-check-218241510.jpg');
-insert into fixture_images
-values ('1549sd64w846', 's2d545ef1121'),
-       ('1549sd64w846', '21554e8f88e4'),
-       ('1549sd64w846', 's4d48e4fe5e4'),
-       ('df4d54g56451', 's2d545ef1121'),
-       ('df4d54g56451', '21554e8f88e4'),
-       ('df4d54g56451', 's4d48e4fe5e4'),
-       ('184fg155485r', 's2d545ef1121'),
-       ('184fg155485r', '21554e8f88e4'),
-       ('184fg155485r', 's4d48e4fe5e4');
+create table posts
+(
+    idpost int NOT NULL auto_increment,
+    name varchar(200),
+    body varchar(1000),
+    primary key (idpost)
+);
