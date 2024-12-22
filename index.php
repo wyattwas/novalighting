fcbd78c0d06a491b210443bd35180aa4bd1586be
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-22 10:11:00 +0100

    refactor

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 <nav class="nav" id="nav">
-  <a href="index.html">Logo</a>
+  <a href="index.php">Logo</a>
   <a href="list-fixture.php">Fixture List</a>
   <a href="past-projects.html">Vergangene Projekte</a>
   <a href="https://www.youtube.com/watch?v=5P1qRpmoFp8&list=PL4nNPjb8kva9e8-wQPH9VLNp214bt5O-_" target="_blank">YouTube</a>
