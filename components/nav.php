31e9df9efea7d0749321433bf3bcec430db33e8e
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-23 18:08:54 +0100

    adding contact

--- components/nav.php
+++ components/nav.php
@@ -3,6 +3,7 @@
     <a href="list-fixture.php">Fixture List</a>
     <a href="list-posts.php">Vergangene Projekte</a>
     <a href="https://www.youtube.com/watch?v=5P1qRpmoFp8&list=PL4nNPjb8kva9e8-wQPH9VLNp214bt5O-_" target="_blank">YouTube</a>
+    <a href="contact.php">Kontakt</a>
     <a href="javascript:void(0);" id="menu-toggle" onclick="togglemenu()">
         <div class="menu-toggle__bar1"></div>
         <div class="menu-toggle__bar2"></div>
---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NovaLighting - Contact</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <meta name="description" content="">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <meta property="og:image:alt" content="">
+
+    <meta name="theme-color" content="#8820ffff">
+</head>
+<body>
+<?php include "components/nav.php"; ?>
+<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; position: relative; top: 100px;">
+    <h1>Kontakt</h1>
+    Zur Zeit ist der beste Weg uns zu kontaktieren über unsere WhatsApp Business Nummer.<br>
+    Nummer:
+</div>
+<?php include "components/footer.php"; ?>
+</body>
+</html>
