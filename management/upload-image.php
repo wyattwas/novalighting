620c19cd1b88d2d7c41164d70e6d4a69b24739b0
wyattwas <wyatt.ruehl@mailbox.org>
2025-03-21 19:05:23 +0100

    fixing image uploads

--- management/upload-image.php
+++ management/upload-image.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['email'])) {
 
 $info = "";
 
-$target_dir = "uploads/";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
