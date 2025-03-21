d59e95e8192caa8ad8f72bbdfa15b2ec9fa934f1
wyattwas <wyatt.ruehl@mailbox.org>
2025-03-21 19:08:30 +0100

    fixing image uploads

--- sql-php/delete-fixture-image.php
+++ sql-php/delete-fixture-image.php
@@ -3,6 +3,6 @@ require('../database.php');
 $fixture_id = $_GET['fixtureid'];
 $image_id = $_GET['imageid'];
 
-$query_images = "DELETE FROM fixture_images WHERE idimage = '".$image_id."' AND idfixture = '".$fixture_id."';";
+$query_images = "DELETE FROM fixture_images WHERE idimage = '$image_id' AND idfixture = '$fixture_id';";
 $stmt = PDO->query($query_images);
 ?>
